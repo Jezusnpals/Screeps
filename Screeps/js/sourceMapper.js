e94ed7abc0f6f079e8740cc601e9a0133a6fdb36
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-03 21:46:44 -0500

    fixed source and spawn mappers to original code

--- Screeps/js/sourceMapper.js
+++ Screeps/js/sourceMapper.js
@@ -1,43 +1,38 @@
 var mapUtils = require('mapUtils');
-var sourceMapper = require('sourceMapper');
-var spawnMapper = require('spawnMapper');
-var creepManager = require('creepManager');
 
-var roomManager =
-{
-    createdConstructionSites: false,
-    initialize: function (room, spawns) {
-        room.memory.initialized = 1;
-        room.memory.mappedSources = [];
-        room.memory.harvestInfos = [];
+var linkingMaxCost = 6;
 
-        var sources = room.find(FIND_SOURCES);
-        for (var index in sources) {
-            var mappedSource = sourceMapper.mapSource(sources[index], spawns);
-            room.memory.mappedSources.push(mappedSource);
+var sourceMapper = {
+    mapSource: function (originalSource) {
+        var mappedSource = {}
+        mappedSource.sourceId = originalSource.id;
+        var collectionPositions = [];
+        var possiblePositions = mapUtils.getAdjacentRoomPositions(originalSource.pos);
+        for (var index in possiblePositions) {
+            if (mapUtils.isWalkableTerrain(possiblePositions[index])) {
+                collectionPositions.push(possiblePositions[index])
+            }
         }
+        mappedSource.collectionPositionInfos = [];
 
-        spawns.forEach(function (spawn) {
-            room.memory.harvestInfos = room.memory.harvestInfos.concat(spawnMapper.mapSpawn(spawn, room.memory.mappedSources));
+
+        collectionPositions.forEach(function (originalPos) {
+            var linkedCollectionPositions = [];
+            collectionPositions.forEach(function (linkPos) {
+                var linkPathResults = mapUtils.getPath(originalPos, linkPos);
+                if (!linkPathResults.incomplete && linkPathResults.cost <= linkingMaxCost) {
+                    linkedCollectionPositions.push(linkPos);
+                }
+            });
+
+            mappedSource.collectionPositionInfos.push({
+                originalPos: originalPos,
+                linkedCollectionPositions: linkedCollectionPositions
+            });
         });
 
-    },
-    run: function (room) {
-
-        if (!room.controller.my) {
-            return;
-        }
-
-        creepManager.run(room);
-
-        if (this.createdConstructionSites) {
-            room.createConstructionSite(25, 25, STRUCTURE_EXTENSION)
-            this.createdConstructionSites = true;
-        }
-        else {
-            room.createConstructionSite(40, 44, STRUCTURE_EXTENSION);
-        }
+        return mappedSource;
     }
 };
 
-module.exports = roomManager;
+module.exports = sourceMapper;
--- Screeps/js/spawnMapper.js
+++ Screeps/js/spawnMapper.js
@@ -1,43 +1,101 @@
 var mapUtils = require('mapUtils');
-var sourceMapper = require('sourceMapper');
-var spawnMapper = require('spawnMapper');
-var creepManager = require('creepManager');
 
-var roomManager =
-{
-    createdConstructionSites: false,
-    initialize: function (room, spawns) {
-        room.memory.initialized = 1;
-        room.memory.mappedSources = [];
-        room.memory.harvestInfos = [];
+var harvestCreepCostToDivisor = 10;
 
-        var sources = room.find(FIND_SOURCES);
-        for (var index in sources) {
-            var mappedSource = sourceMapper.mapSource(sources[index], spawns);
-            room.memory.mappedSources.push(mappedSource);
-        }
+function getHarvestInfo(spawnPosition, collectionPosition, sourceId, spawnId, linkedCollectionPositions) {
+    var harvestInfo = {
+        spawnPosition: spawnPosition,
+        collectionPosition: collectionPosition,
+        creepIds: [],
+        pathTo: [],
+        costTo: -1,
+        returnPath: [],
+        isSeperateReturnPath: false,
+        returnPathBlockers: [],
+        canGetTo: false,
+        maxHarvesters: 0,
+        sourceId: sourceId,
+        spawnId: spawnId,
+        linkedCollectionPositions: linkedCollectionPositions
+    };
 
-        spawns.forEach(function (spawn) {
-            room.memory.harvestInfos = room.memory.harvestInfos.concat(spawnMapper.mapSpawn(spawn, room.memory.mappedSources));
-        });
+    var pathToResults = mapUtils.getPath(spawnPosition, collectionPosition);
 
-    },
-    run: function (room) {
+    if (!pathToResults.incomplete) {
+        harvestInfo.pathTo = pathToResults.path;
+        harvestInfo.costTo = pathToResults.cost;
+        harvestInfo.canGetTo = true;
 
-        if (!room.controller.my) {
-            return;
-        }
+        var pathToAvoid = pathToResults.path.slice();
+        mapUtils.removeRoomPositionFromArray(pathToAvoid, spawnPosition);
+        mapUtils.removeRoomPositionFromArray(pathToAvoid, collectionPosition);
 
-        creepManager.run(room);
+        var pathFromResults = mapUtils.getPath(collectionPosition, spawnPosition, pathToAvoid);
 
-        if (this.createdConstructionSites) {
-            room.createConstructionSite(25, 25, STRUCTURE_EXTENSION)
-            this.createdConstructionSites = true;
+        if (pathFromResults.incomplete) {
+            harvestInfo.isSeperateReturnPath = false;
+
+            pathFromResults = mapUtils.getPath(collectionPosition, spawnPosition, [], pathToAvoid);
+            harvestInfo.returnPathBlockers = mapUtils.getSameRoomPositionsFromArray(pathFromResults.path, pathToAvoid);
+            harvestInfo.pathFrom = pathFromResults.path;
         }
         else {
-            room.createConstructionSite(40, 44, STRUCTURE_EXTENSION);
+            harvestInfo.pathFrom = pathFromResults.path;
+            harvestInfo.isSeperateReturnPath = true;
+            harvestInfo.maxHarvesters = 1 + Math.floor(harvestInfo.costTo / harvestCreepCostToDivisor);
         }
     }
-};
 
-module.exports = roomManager;
+    return harvestInfo;
+}
+
+
+function calculateNumberOfCreepsForNoReturnPath(infosWithoutReturnPath) {
+    while (infosWithoutReturnPath.length > 0) {
+        var collidingInfos = [];
+        collidingInfos.push({
+            info: infosWithoutReturnPath[0],
+            index: 0
+        });
+        var collidingBlockingPoints = 0;
+        for (var i = 1; i < infosWithoutReturnPath.length; i++) {
+            var sameBlockingPoints = getSameRoomPositionsFromArray(infosWithoutReturnPath[0].returnPathBlockers, infosWithoutReturnPath[i].returnPathBlockers)
+            if (sameBlockingPoints.length > 0) {
+                collidingBlockingPoints += sameBlockingPoints.length;
+                collidingInfos.push({
+                    info: infosWithoutReturnPath[i],
+                    index: i
+                });
+            }
+        }
+
+        collidingInfos.forEach(function (infoIndexPair) {
+            var harvestCreepCostFromDivisor = harvestCreepCostToDivisor + (collidingInfos.length * collidingBlockingPoints);
+            infoIndexPair.info.maxHarvesters = 1 + Math.floor(infoIndexPair.info.costTo / harvestCreepCostFromDivisor);
+            infosWithoutReturnPath.splice(infoIndexPair.index, 1);
+        })
+    }
+}
+
+
+var spawnMapper = {
+    mapSpawn: function (spawn, mappedSources) {
+
+        var harvestInfos = [];
+
+        mappedSources.forEach(function (mappedSource) {
+            mappedSource.collectionPositionInfos.forEach(function (collectionPositionInfo) {
+                harvestInfos.push(getHarvestInfo(spawn.pos, collectionPositionInfo.originalPos, mappedSource.sourceId, spawn.id, collectionPositionInfo.linkedCollectionPositions))
+            });
+
+        });
+
+        var infosWithoutReturnPath = harvestInfos.filter(info => !info.isSeperateReturnPath);
+        calculateNumberOfCreepsForNoReturnPath(infosWithoutReturnPath);
+
+        return harvestInfos;
+    }
+}
+
+module.exports = spawnMapper;
+
