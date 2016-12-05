b5089bce1a3fbf99471bfaf146616d10edac7a56
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-05 00:54:21 -0500

    added upgrader creation logic

--- Screeps/js/creepManager.js
+++ Screeps/js/creepManager.js
@@ -6,43 +6,65 @@ function calculateHarvestCost(info) {
 
 var creepManager =
 {
-    calculateHarvestSource: function (harvestInfos) {
-        var openHarvestInfos = harvestInfos.filter(info => info.creepNames.length < info.maxCreeps); //maxCreeps
-        if (openHarvestInfos.length == 0) {
+    calculateBestSource: function (infos) {
+        var openInfos = infos.filter(info => info.creepNames.length < info.maxCreeps); //maxCreeps
+        if (openInfos.length == 0) {
             return null;
         }
 
-        var lowestCost = calculateHarvestCost(openHarvestInfos[0]);
+        var lowestCost = calculateHarvestCost(openInfos[0]);
         var lowestCostIndex = 0;
 
-        for (var i = 1; i < openHarvestInfos.length; i++) {
-            var currentCost = calculateHarvestCost(openHarvestInfos[i]);
+        for (var i = 1; i < openInfos.length; i++) {
+            var currentCost = calculateHarvestCost(openInfos[i]);
             if (currentCost < lowestCost) {
                 lowestCost = currentCost;
                 lowestCostIndex = i;
             }
         }
 
-        return openHarvestInfos[lowestCostIndex];
+        return openInfos[lowestCostIndex];
     },
     run: function (room) {
         if (Object.keys(Game.creeps).length < 200 && Game.spawns['Spawn1'].energy >= 200)
         {
-            if (room.memory.harvestInfos)
+            if (Object.keys(Game.creeps).length % 2 == 0)
             {
-                var bestHarvestInfo = this.calculateHarvestSource(room.memory.harvestInfos);
-                if (bestHarvestInfo != null)
+                if (room.memory.harvestInfos)
                 {
-                    var bestHarvestInfoIndex = room.memory.harvestInfos.indexOf(bestHarvestInfo);
-                    var creepName = 'H' + new Date().getTime();
-                    var creepResult = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], creepName, { behavior: behaviorEnum.HARVESTER, harvestInfoIndex: bestHarvestInfoIndex });
-                    if (creepResult == creepName)
+                    var bestHarvestInfo = this.calculateBestSource(room.memory.harvestInfos);
+                    if (bestHarvestInfo != null)
                     {
-                        room.memory.harvestInfos[bestHarvestInfoIndex].creepNames.push(creepName);
+                        var bestHarvestInfoIndex = room.memory.harvestInfos.indexOf(bestHarvestInfo);
+                        var creepName = 'H' + new Date().getTime();
+                        var creepResult = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], creepName, { behavior: behaviorEnum.HARVESTER, harvestInfoIndex: bestHarvestInfoIndex });
+                        if (creepResult == creepName)
+                        {
+                            room.memory.harvestInfos[bestHarvestInfoIndex].creepNames.push(creepName);
+                        }
                     }
                 }
             }
         }
+        else
+        {
+            if (room.memory.controlInfos)
+                {
+                var bestControlInfo = this.calculateBestSource(room.memory.controlInfos);
+                if (bestControlInfo != null)
+                    {
+                    var bestControlInfoIndex = room.memory.controlInfos.indexOf(bestControlInfoIndex);
+                        var creepName = 'U' + new Date().getTime();
+                        var creepResult = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], creepName, { behavior: behaviorEnum.UPGRADER, controlInfoIndex: bestControlInfoIndex });
+                        if (creepResult == creepName)
+                        {
+                            room.memory.controlInfos[bestControlInfoIndex].creepNames.push(creepName);
+                        }
+                    }
+                }
+        }
+            
+        
     }
 }
 module.exports = creepManager;
