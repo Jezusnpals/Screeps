a8bea8fded9c5794d0da103ddc37e893777bbbf6
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-04 23:14:08 -0500

    Refactored logic out of spawnMapper to a generic infoMapper

---
+++ Screeps/js/infoMapper.js
@@ -0,0 +1,74 @@
+var mapUtils = require('mapUtils');
+var pathManager = require('pathManager');
+
+var infoMapper = {
+    calculateMappedInfo: function (startPosition, collectionPosition) {
+        var mappedInfo = {
+            startPosition: startPosition,
+            collectionPosition: collectionPosition,
+            pathToId: -1,
+            costTo: -1,
+            isSeperateReturnPath: false,
+            returnPathBlockers: [],
+            canGetTo: false,
+            pathFromId: -1
+        };
+
+        var pathToResults = mapUtils.findPath(startPosition, collectionPosition);
+
+        if (!pathToResults.incomplete) {
+            mappedInfo.pathToId = pathManager.addHarvestPathTo(pathToResults.path);
+            mappedInfo.costTo = pathToResults.cost;
+            mappedInfo.canGetTo = true;
+
+            var pathToAvoid = pathToResults.path.slice();
+            mapUtils.removeRoomPositionFromArray(pathToAvoid, startPosition);
+            mapUtils.removeRoomPositionFromArray(pathToAvoid, collectionPosition);
+
+            var pathFromResults = mapUtils.findPath(collectionPosition, startPosition, pathToAvoid);
+
+            if (pathFromResults.incomplete) {
+                mappedInfo.isSeperateReturnPath = false;
+
+                pathFromResults = mapUtils.findPath(collectionPosition, startPosition, [], pathToAvoid);
+                mappedInfo.returnPathBlockers = mapUtils.getSameRoomPositionsFromArray(pathFromResults.path, pathToAvoid);
+                mappedInfo.pathFromId = pathManager.addHarvestPathFrom(pathFromResults.path);
+            }
+            else {
+                mappedInfo.pathFromId = pathManager.addHarvestPathFrom(pathFromResults.path);
+                mappedInfo.isSeperateReturnPath = true;
+            }
+        }
+
+        return mappedInfo;
+    },
+    calculateHarvestSource: function (infosWithoutReturnPath, baseCreepCostDivisor) {
+        while (infosWithoutReturnPath.length > 0) {
+            var collidingInfos = [];
+            collidingInfos.push({
+                info: infosWithoutReturnPath[0],
+                index: 0
+            });
+            var collidingBlockingPoints = 0;
+            for (var i = 1; i < infosWithoutReturnPath.length; i++) {
+                var sameBlockingPoints = getSameRoomPositionsFromArray(infosWithoutReturnPath[0].returnPathBlockers, infosWithoutReturnPath[i].returnPathBlockers)
+                if (sameBlockingPoints.length > 0) {
+                    collidingBlockingPoints += sameBlockingPoints.length;
+                    collidingInfos.push({
+                        info: infosWithoutReturnPath[i],
+                        index: i
+                    });
+                }
+            }
+
+            collidingInfos.forEach(function (infoIndexPair) {
+                var creepCostFromDivisor = baseCreepCostDivisor + (collidingInfos.length * collidingBlockingPoints);
+                infoIndexPair.info.maxCreeps = 1 + Math.floor(infoIndexPair.info.costTo / creepCostFromDivisor);
+                infosWithoutReturnPath.splice(infoIndexPair.index, 1);
+            })
+        }
+    }
+    };
+
+module.exports = infoMapper;
+
--- Screeps/js/spawnMapper.js
+++ Screeps/js/spawnMapper.js
@@ -1,86 +1,16 @@
 var mapUtils = require('mapUtils');
 var pathManager = require('pathManager');
+var infoMapper = require('infoMapper');
 
 var harvestCreepCostToDivisor = 12;
 
-function calculateMappedInfo(startPosition, collectionPosition) {
-    var mappedInfo = {
-        startPosition: startPosition,
-        collectionPosition: collectionPosition,
-        pathToId: -1,
-        costTo: -1,
-        isSeperateReturnPath: false,
-        returnPathBlockers: [],
-        canGetTo: false,
-        pathFromId: -1
-    };
-
-    var pathToResults = mapUtils.findPath(startPosition, collectionPosition);
-
-    if (!pathToResults.incomplete) {
-        mappedInfo.pathToId = pathManager.addHarvestPathTo(pathToResults.path);
-        mappedInfo.costTo = pathToResults.cost;
-        mappedInfo.canGetTo = true;
-
-        var pathToAvoid = pathToResults.path.slice();
-        mapUtils.removeRoomPositionFromArray(pathToAvoid, startPosition);
-        mapUtils.removeRoomPositionFromArray(pathToAvoid, collectionPosition);
-
-        var pathFromResults = mapUtils.findPath(collectionPosition, startPosition, pathToAvoid);
-
-        if (pathFromResults.incomplete) {
-            mappedInfo.isSeperateReturnPath = false;
-
-            pathFromResults = mapUtils.findPath(collectionPosition, startPosition, [], pathToAvoid);
-            mappedInfo.returnPathBlockers = mapUtils.getSameRoomPositionsFromArray(pathFromResults.path, pathToAvoid);
-            mappedInfo.pathFromId = pathManager.addHarvestPathFrom(pathFromResults.path);
-        }
-        else {
-            mappedInfo.pathFromId = pathManager.addHarvestPathFrom(pathFromResults.path);
-            mappedInfo.isSeperateReturnPath = true;
-        }
-    }
-
-    return mappedInfo;
-}
-
-
-function calculateNumberOfCreepsForNoReturnPath(infosWithoutReturnPath, baseCreepCostDivisor) {
-    while (infosWithoutReturnPath.length > 0) {
-        var collidingInfos = [];
-        collidingInfos.push({
-            info: infosWithoutReturnPath[0],
-            index: 0
-        });
-        var collidingBlockingPoints = 0;
-        for (var i = 1; i < infosWithoutReturnPath.length; i++) {
-            var sameBlockingPoints = getSameRoomPositionsFromArray(infosWithoutReturnPath[0].returnPathBlockers, infosWithoutReturnPath[i].returnPathBlockers)
-            if (sameBlockingPoints.length > 0) {
-                collidingBlockingPoints += sameBlockingPoints.length;
-                collidingInfos.push({
-                    info: infosWithoutReturnPath[i],
-                    index: i
-                });
-            }
-        }
-
-        collidingInfos.forEach(function (infoIndexPair) {
-            var creepCostFromDivisor = baseCreepCostDivisor +(collidingInfos.length * collidingBlockingPoints);
-            infoIndexPair.info.maxCreeps = 1 + Math.floor(infoIndexPair.info.costTo / creepCostFromDivisor);
-            infosWithoutReturnPath.splice(infoIndexPair.index, 1);
-        })
-    }
-}
-
-
 var spawnMapper = {
     mapSpawn: function (spawn, mappedSources) {
-
         var harvestInfos = [];
 
         mappedSources.forEach(function (mappedSource) {
             mappedSource.collectionPositionInfos.forEach(function (collectionPositionInfo) {
-                var mappedInfo = calculateMappedInfo(spawn.pos, collectionPositionInfo.originalPos);
+                var mappedInfo = infoMapper.calculateMappedInfo(spawn.pos, collectionPositionInfo.originalPos);
                 collectionPositionInfo.harvestPathFromId = mappedInfo.pathFromId;
                 var harvestInfo = {
                     sourceId: mappedSource.sourceId,
@@ -98,11 +28,10 @@ var spawnMapper = {
                 }
                 harvestInfos.push(harvestInfo);
             });
-
         });
 
         var infosWithoutReturnPath = harvestInfos.filter(info => !info.isSeperateReturnPath);
-        calculateNumberOfCreepsForNoReturnPath(infosWithoutReturnPath, harvestCreepCostToDivisor);
+        infoMapper.calculateNumberOfCreepsForNoReturnPath(infosWithoutReturnPath, harvestCreepCostToDivisor);
 
         return harvestInfos;
     }
