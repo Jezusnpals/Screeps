1869eb46407c01f8d8d28fc1771b432823225689
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-05 15:37:28 -0500

    changed upgrader logic

--- Screeps/js/behavior.upgrader.js
+++ Screeps/js/behavior.upgrader.js
@@ -37,33 +37,26 @@ var upgrader =
     {
     run: function (creep)
     {
-        var creepNeedsEnergy = creep.carry.energy < creep.carryCapacity;
-        var creepControlInfo = creep.memory.controlInfoIndex >= 0 ? creep.room.memory.controlInfos[creep.memory.controlInfoIndex] : null;
-        if (creepNeedsEnergy)
+
+        if (creep.memory.upgrading && creep.carry.energy == 0)
         {
-            creepUtils.harvestEnergy(creep, creepControlInfo);
+            creep.memory.upgrading = false;
+            }
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity)
+        {
+            creep.memory.upgrading = true;
         }
-        else
+
+        var creepControlInfo = creep.memory.controlInfoIndex >= 0 ? creep.room.memory.controlInfos[creep.memory.controlInfoIndex] : null;
+        if (creep.memory.upgrading)
         {
             transferEnergy(creep, creepControlInfo);
         }
+        else
+        {
+            creepUtils.harvestEnergy(creep, creepControlInfo);
+        }
         
-        /*
-        if (creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('harvesting');
-        }
-        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
-            creep.say('upgrading');
-        }
-
-        if (creep.memory.upgrading) {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-        */
     }
 };
 
