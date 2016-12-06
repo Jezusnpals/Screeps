dcd8ab39a62c1f992a92114d8b2cfd57cd1e0d86
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-06 00:20:06 -0500

    flag bugs

--- Screeps/js/flagDrawer.js
+++ Screeps/js/flagDrawer.js
@@ -10,18 +10,17 @@ var flagDrawer = {
     {
         if (showFlagsForPaths) {
             room.memory.harvestInfos.forEach(function (info, i) {
-                    pathManager.getPathTo(info.pathToId).forEach(function (pointOnPath, j) {
-                        var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
-                        //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
-                        room.createFlag(pointOnPath, 't' + +i + j, colors[1]);
-                    });
+                pathManager.getPathTo(info.pathToId).forEach(function (pointOnPath, j) {
+                    var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
+                    //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
+                    room.createFlag(pointOnPath, 't' + +i + j, colors[1]);
+                });
 
-                    info[showFlagForHarvestInfo].forEach(function (pointOnPath, k) {
-                        var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
-                        //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
-                        room.createFlag(pointOnPath, 'f' + +i + j, colors[0]);
-                    });
-                })
+                info[showFlagForHarvestInfo].forEach(function (pointOnPath, k) {
+                    var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
+                    //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
+                    room.createFlag(pointOnPath, 'f' + +i + j, colors[0]);
+                });
             });
         }
         else if (showFlagsForSourcePoints) {
