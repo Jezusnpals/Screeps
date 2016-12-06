a8342a0a1535e02b78bb6b888198e3efa75f8813
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-06 00:30:25 -0500

    add from path flags

--- Screeps/js/flagDrawer.js
+++ Screeps/js/flagDrawer.js
@@ -1,33 +1,41 @@
 var pathManager = require('pathManager');
 
-var showFlagsForPaths = true;
+var showFlagsForToPaths = true;
 var showFlagsForSourcePoints = false;
-
+var showFlagsForFromPaths = true;
 
 var flagDrawer = {
     showFlags: function(room)
     {
-        if (showFlagsForPaths) {
+        if (showFlagsForToPaths) {
             room.memory.harvestInfos.forEach(function (info, i) {
                 pathManager.getPathTo(info.pathToId).forEach(function (pointOnPath, j) {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
-                    //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
-                    room.createFlag(pointOnPath, 't' + +i + j, colors[1]);
+                    room.createFlag(pointOnPath, 't' +i + j, colors[1]);
                 });
 
                 info.returnPathBlockers.forEach(function (pointOnPath, j) {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
-                    //room.createFlag(pointOnPath, 'f' + + i + j + k, colors[i % colors.length]);
-                    room.createFlag(pointOnPath, 'f' + +i + j, colors[0]);
+                    room.createFlag(pointOnPath, 'f' +i + j, colors[0]);
                 });
+
+                
             });
         }
-        else if (showFlagsForSourcePoints) {
+        if (showFlagsForSourcePoints)
+        {
             room.memory.mappedSources.forEach(function (value, i) {
                 value.collectionPositions.forEach(function (pos, j) {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE]
-                    room.createFlag(pos, 'p' + +i + j, colors[i % colors.length]);
+                    room.createFlag(pos, 'p' +i + j, colors[2]);
                 });
+            });
+        }
+        if (showFlagsForFromPaths)
+        {
+            Memory.pathManager.pathFromList.forEach(function (pointOnPath, i) {
+                var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
+                room.createFlag(pointOnPath, 'f' +i , colors[3]);
             });
         }
     }
