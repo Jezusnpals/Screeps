c04f178d6cfdc7c950f433ad12c74742d5f0b03b
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-06 19:43:57 -0500

    added refreshing positions for flag creation

--- Screeps/js/flagDrawer.js
+++ Screeps/js/flagDrawer.js
@@ -9,14 +9,17 @@ var flagDrawer = {
     showFlags: function(room)
     {
         if (showFlagsForToPaths) {
-            room.memory.harvestInfos.forEach(function (info, i) {
-                pathManager.getPathTo(info.pathToId).forEach(function (pointOnPath, j) {
+            room.memory.harvestInfos.forEach(function (info, i)
+            {
+                pathManager.getPathTo(info.pathToId).forEach(function (pointOnPath, j)
+                {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
                     room.createFlag(pointOnPath, 't' +i + j, colors[1]);
                 });
 
 
-                info.returnPathBlockers.forEach(function (pointOnPath, j) {
+                mapUtils.refreshRoomPositionArray(info.returnPathBlockers).forEach(function (pointOnPath, j)
+                {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
                     room.createFlag(pointOnPath, 'f' +i + j, colors[0]);
                 });
@@ -28,7 +31,7 @@ var flagDrawer = {
                 });
 
 
-                info.returnPathBlockers.forEach(function (pointOnPath, j) {
+                mapUtils.refreshRoomPositionArray(info.returnPathBlockers).forEach(function (pointOnPath, j) {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE];
                     room.createFlag(pointOnPath, 'cf' + i + j, colors[5]);
                 });
@@ -37,7 +40,7 @@ var flagDrawer = {
         if (showFlagsForSourcePoints)
         {
             room.memory.mappedSources.forEach(function (value, i) {
-                value.collectionPositions.forEach(function (pos, j) {
+                mapUtils.refreshRoomPositionArray(value.collectionPositions).forEach(function (pos, j) {
                     var colors = [COLOR_BLUE, COLOR_RED, COLOR_YELLOW, COLOR_ORANGE, COLOR_WHITE, COLOR_GREEN, COLOR_PURPLE]
                     room.createFlag(pos, 'p' +i + j, colors[2]);
                 });
