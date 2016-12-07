bb5ac9341c1d3d0b4da9fb4d23ddf5c621abbfa8
Jacob Ellenberg <JakeEllenberg@gmail.com>
2016-12-06 22:05:19 -0500

    fixed issue in distance forumla

--- Screeps/js/mapUtils.js
+++ Screeps/js/mapUtils.js
@@ -89,7 +89,7 @@ var mapUtils = {
     },
     calculateDistanceBetweenTwoPoints: function(pos1, pos2)
     {
-        return Math.sqrt(Math.pow((pos1.x - pos2.x), 2) + Math.pow((pos1.y + pos2.y), 2));
+        return Math.sqrt(Math.pow((pos1.x - pos2.x), 2) + Math.pow((pos1.y - pos2.y), 2));
     }
 };
 
