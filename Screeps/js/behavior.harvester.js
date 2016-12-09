104f092d759114ff662812430c710763b74c2599
Jezusnpals <JakeEllenberg@gmail.com>
2016-12-09 15:14:24 -0500

    fixed issue in behavior harvester where when there wasn't a harvest info he couldnt find a spawn structure. Also refactored creep harvest info so we didnt have to calculate it again in the transfer energy function.

--- Screeps/js/behavior.harvester.js
+++ Screeps/js/behavior.harvester.js
@@ -1,14 +1,13 @@
 var pathManager = require('pathManager');
 var creepUtils = require('creepUtils');
 
-function transferEnergy(creep)
+function transferEnergy(creep, creepHarvestInfo)
 {
-    var creepHarvestInfo = creep.memory.harvestInfoIndex >= 0 ? creep.room.memory.harvestInfos[creep.memory.harvestInfoIndex] : null;
     var structure = creepHarvestInfo ? Game.getObjectById(creepHarvestInfo.spawnId) :
-        creep.room.find(STRUCTURE_SPAWN)[0];
+        creep.room.find(FIND_STRUCTURES).filter(s => s.structureType == STRUCTURE_SPAWN)[0];
     var transferResults = creep.transfer(structure, RESOURCE_ENERGY);
 
-    if(transferResults = ERR_NOT_IN_RANGE)
+    if(transferResults == ERR_NOT_IN_RANGE)
     {
         if(creepHarvestInfo)
         {
@@ -26,14 +25,14 @@ var harvester =
     run: function (creep)
     {
         var creepNeedsEnergy = creep.carry.energy < creep.carryCapacity;
+        var creepHarvestInfo = creep.memory.harvestInfoIndex >= 0 ? creep.room.memory.harvestInfos[creep.memory.harvestInfoIndex] : null;
         if (creepNeedsEnergy)
         {
-            var creepHarvestInfo = creep.memory.harvestInfoIndex >= 0 ? creep.room.memory.harvestInfos[creep.memory.harvestInfoIndex] : null;
             creepUtils.harvestEnergy(creep, creepHarvestInfo);
         }
         else 
         {
-            transferEnergy(creep);
+            transferEnergy(creep, creepHarvestInfo);
         }
     }
 };
